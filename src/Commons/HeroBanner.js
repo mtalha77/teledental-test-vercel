7776fe947087f5e1c81260de14c85b50a7be699b
talha <mtalha77@gmail.com>
2025-04-04 02:13:19 +0500

    add hero banner changes

--- src/Commons/HeroBanner.js
+++ src/Commons/HeroBanner.js
@@ -1,9 +1,6 @@
 import React from "react";
-import heroBannerIcon from "../assets/img/hm_banner_img1.webp";
-//import heroBannerIcon from "../assets/img/hm_banner_img1_1.webp";
 import SignInModal from "../Auth/SignInModal";
 import { Link } from "react-router-dom";
-import tickIcon from "../assets/svg/arrow-right-sm-svgrepo-com.svg";
 const HeroBanner = () => {
   const [isSignInModalVisible, setIsSignInModalVisible] = React.useState(false);
   const [isVerificationModalVisible, setIsVerificationModalVisible] =
@@ -11,7 +8,15 @@ const HeroBanner = () => {
   const [entity, setEntity] = React.useState("");
   return (
     <>
-      <div className="uui-page-padding-18">
+      <div
+        className="uui-page-padding-18"
+        style={{
+          backgroundImage: "url('/images/hero-banner-bg-image.png')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
         <div className="autoContent">
           <div className="uui-padding-vertical-xhuge-16">
             <div className="uui-heroheader01_component-4">
@@ -20,80 +25,11 @@ const HeroBanner = () => {
                 className="uui-heroheader01_content-4 order_change_res"
               >
                 <h1 className="uui-heading-xlarge-8 uui_heading_hero">
-                  <span className="d-block">Talk with a Live Dentist on</span>
-                  <label>Teledental.com </label>
+                  <span>Talk with a Live Dentist on</span>
+                  <label className="d-inline">Teledental.com </label>
 
                   <small> Anywhere, 24/7</small>
                 </h1>
-                <div className="brix---buttons-row hero_banner_btn">
-                  <div className="brix---button-row-left">
-                    <Link
-                      to="/patient-signup"
-                      name="patient"
-                      className="brix---btn-primary w-button btn-edit prim_btn_blue-fill"
-                      // onClick={() => setIsSignInModalVisible(true)}
-                    >
-                      Start Here
-                    </Link>
-                  </div>
-                  <Link
-                    to="/contact-us"
-                    className="brix---btn-primary w-button btn-edit prim_btn_blue-fill"
-                  >
-                    Contact Us
-                  </Link>
-                </div>
-                <div className="uui-layout08_item-4">
-                  <div className="uui-layout08_item-icon-wrapper-4">
-                    <div className="uui-icon-1x1-xsmall-11 w-embed">
-                      <img src={tickIcon} alt="tick icon" />
-                    </div>
-                  </div>
-                  <div className="uui-layout08_item-text-wrapper-3">
-                    <div className="uui-text-size-large-11">
-                      Professional <strong>online dental consultation</strong>{" "}
-                      with virtual dentists.
-                    </div>
-                  </div>
-                </div>
-                <div className="uui-layout08_item-4">
-                  <div className="uui-layout08_item-icon-wrapper-4">
-                    <div className="uui-icon-1x1-xsmall-11 w-embed">
-                      <img src={tickIcon} alt="tick icon" />
-                    </div>
-                  </div>
-                  <div className="uui-layout08_item-text-wrapper-3">
-                    <div className="uui-text-size-large-11">
-                      Get <strong>virtual dental help</strong> and find{" "}
-                      <strong>local dentists near you</strong>.
-                    </div>
-                  </div>
-                </div>
-                <div className="uui-layout08_item-4">
-                  <div className="uui-layout08_item-icon-wrapper-4">
-                    <div className="uui-icon-1x1-xsmall-11 w-embed">
-                      <img src={tickIcon} alt="tick icon" />
-                    </div>
-                  </div>
-                  <div className="uui-layout08_item-text-wrapper-3">
-                    <div className="uui-text-size-large-11">
-                      Schedule a <strong>live dental video consult</strong> with
-                      a Teledental dentist.
-                    </div>
-                  </div>
-                </div>
-                <div className="uui-layout08_item-4">
-                  <div className="uui-layout08_item-icon-wrapper-4">
-                    <div className="uui-icon-1x1-xsmall-11 w-embed">
-                      <img src={tickIcon} alt="tick icon" />
-                    </div>
-                  </div>
-                  <div className="uui-layout08_item-text-wrapper-3">
-                    <div className="uui-text-size-large-11">
-                      Schedule a dental appointment.
-                    </div>
-                  </div>
-                </div>
 
                 <div className="uui-space-large-8"></div>
                 <div className="brix---buttons-row hero_banner_btn">
@@ -114,11 +50,11 @@ const HeroBanner = () => {
                 data-aos="fade-left"
                 // data-aos-duration="500"
               >
-                {/* <h3 className="tagLineText">
-                  Live <span>Teledental</span> <br/> Consultation
-                </h3> */}
+                <h3 className="tagLineText">
+                  Live <span>Teledental</span> Consultation
+                </h3>
                 <img
-                  src={heroBannerIcon}
+                  src={"/images/banner-dentist.png"}
                   loading="lazy"
                   alt="Header image"
                   className="uui-heroheader01_image-5"
