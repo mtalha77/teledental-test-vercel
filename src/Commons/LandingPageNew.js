8d1411cc5951aac3590b58f4716932498cb011b4
talha <mtalha77@gmail.com>
2025-04-05 04:01:46 +0500

    remove unnecessary code

--- src/Commons/LandingPageNew.js
+++ src/Commons/LandingPageNew.js
@@ -9,10 +9,6 @@ import appStore from "../assets/img/app-store.webp";
 import playStore from "../assets/img/play-store.webp";
 import SignInModal from "../Auth/SignInModal";
 import Header from "./Header";
-import ConnectDentist from "../Commons/ConnectDentist/ConnectDentist";
-import HomeFaqs from "../Commons/HomeFaqs/HomeFaqs";
-import HomeServices from "./HomeServices/HomeServices";
-import HomeJoinUs from "./HomeJoinUs/HomeJoinUs";
 import HowItWorks from "./HowItWorks";
 import DentalGuidance from "./dental-guidance";
 import ProviderCarousel from "./provider-carousel";
@@ -50,43 +46,43 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function LandingPageNew() {
-  const classes = useStyles();
-  const featureItems = [
-    "TeleDental Chat with dentists in real-time.",
-    `24/7 Anytime, from any place.`,
-    `Teledental care - virtual dentistry`,
-    `Find dentists near you`,
-  ];
+  // const classes = useStyles();
+  // const featureItems = [
+  //   "TeleDental Chat with dentists in real-time.",
+  //   `24/7 Anytime, from any place.`,
+  //   `Teledental care - virtual dentistry`,
+  //   `Find dentists near you`,
+  // ];
 
-  const featureItems2 = [
-    "Left Teeth",
-    `Common Ailments`,
-    `Third Molars / Wisdom Teeth`,
-    `ProblemsTooth abscessSmall`,
-    `Tooth cavityLarge tooth`,
-    `Cavity gum problemBreak a`,
-    `ToothTooth extraction, dry socket`,
-  ];
+  // const featureItems2 = [
+  //   "Left Teeth",
+  //   `Common Ailments`,
+  //   `Third Molars / Wisdom Teeth`,
+  //   `ProblemsTooth abscessSmall`,
+  //   `Tooth cavityLarge tooth`,
+  //   `Cavity gum problemBreak a`,
+  //   `ToothTooth extraction, dry socket`,
+  // ];
 
-  const returnFeatureItem = (featureText, index) => {
-    return (
-      <div
-        className={`d-flex ${
-          index !== 0 ? "featureItem" : ""
-        } featureItemForAll`}
-        key={featureText}
-      >
-        <div
-          className={`d-flex justify-content-center align-items-start mr-3 featureItemImg`}
-        >
-          <img src={toothImage} alt={`Tooth`} />
-        </div>
-        <div className="blue text-left text-break">{featureText}</div>
-      </div>
-    );
-  };
+  // const returnFeatureItem = (featureText, index) => {
+  //   return (
+  //     <div
+  //       className={`d-flex ${
+  //         index !== 0 ? "featureItem" : ""
+  //       } featureItemForAll`}
+  //       key={featureText}
+  //     >
+  //       <div
+  //         className={`d-flex justify-content-center align-items-start mr-3 featureItemImg`}
+  //       >
+  //         <img src={toothImage} alt={`Tooth`} />
+  //       </div>
+  //       <div className="blue text-left text-break">{featureText}</div>
+  //     </div>
+  //   );
+  // };
 
-  const history = useHistory();
+  // const history = useHistory();
   const [isSignInModalVisible, setIsSignInModalVisible] = React.useState(false);
   const [isVerificationModalVisible, setIsVerificationModalVisible] =
     React.useState(false);
@@ -101,14 +97,14 @@ function LandingPageNew() {
       </section>
 
       <HowItWorks />
-      
+
       <DentalGuidance />
-      
+
       <ProviderCarousel />
-      
+
       <Faq />
-      
-      <AppointmentBooking/>
+
+      <AppointmentBooking />
 
       <section className="uui-section_heroheader07-2">
         <div className="uui-page-padding-28">
