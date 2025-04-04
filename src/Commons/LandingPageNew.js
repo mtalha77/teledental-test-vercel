d0b91598f2fd4d6ace598a150754f5fdebfac3fd
talha <mtalha77@gmail.com>
2025-04-05 02:46:14 +0500

    add booking section

--- src/Commons/LandingPageNew.js
+++ src/Commons/LandingPageNew.js
@@ -17,6 +17,7 @@ import HowItWorks from "./HowItWorks";
 import DentalGuidance from "./dental-guidance";
 import ProviderCarousel from "./provider-carousel";
 import Faq from "./faq";
+import AppointmentBooking from "./appointment-booking";
 
 const useStyles = makeStyles((theme) => ({
   mainLogo: {
@@ -105,8 +106,9 @@ function LandingPageNew() {
       
       <ProviderCarousel />
       
-      <Faq/>
-
+      <Faq />
+      
+      <AppointmentBooking/>
 
       <section className="uui-section_heroheader07-2">
         <div className="uui-page-padding-28">
